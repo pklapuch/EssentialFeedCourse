3a8aaf2ef13110b0b2709d87c18463c4b7ea7afe
Pawel Klapuch <pawelklapuch@gmail.com>
2023-03-25 17:32:56 -0500

    Create HTTPClient and RemoteFeedLoader in Test target

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,13 +8,23 @@
 import XCTest
 
 class RemoteFeedLoader {
+    private let client: HTTPClient
+    
     init(client: HTTPClient) {
-        
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://www.test.com")!)
     }
 }
 
 class HTTPClient {
     private(set) var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
@@ -23,5 +33,14 @@ final class RemoteFeedLoaderTests: XCTestCase {
         _ = RemoteFeedLoader(client: client)
 
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient()
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
