93091a9348f8d495baf4bded6ef26242f0107ed0
Pawel Klapuch <pawelklapuch@gmail.com>
2023-03-25 14:01:00 -0500

    Created `FeedItem` model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Pawel Klapuch on 3/25/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUR: URL
+}
