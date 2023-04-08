eaf4e7f5c82724d1540b481c1c5eeb93e025790e
Pawel Klapuch <pawelklapuch@gmail.com>
2023-04-08 11:40:45 +0200

    Add memory leak detection

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -30,14 +30,19 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if let items = try? FeedItemsMapper.map(data, response) {
-                    completion(.success(items))
-                } else {
-                    completion(.failure(.invalidData))
-                }
+                completion(Self.map(data, response))
             case .failure:
                 completion(.failure(.connectivity))
             }
+        }
+    }
+    
+    private static func map(_ data: Data, _ resposne: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, resposne)
+            return .success(items)
+        } catch {
+            return .failure(Error.invalidData)
         }
     }
 }
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -99,11 +99,22 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!,
+                         file: StaticString = #file,
+                         line: UInt = #line) -> (RemoteFeedLoader, HTTPClientSpy) {
         let spy = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: spy)
         
+        trackForMmeoryLeaks(spy)
+        trackForMmeoryLeaks(sut)
+        
         return (sut, spy)
+    }
+    
+    private func trackForMmeoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak", file: file, line: line)
+        }
     }
     
     private func expect(_ sut: RemoteFeedLoader,
