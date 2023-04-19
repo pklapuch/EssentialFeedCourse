65867593e013d2026a4e4bfb795f9c7fd52e10a2
Pawel Klapuch <pawelklapuch@gmail.com>
2023-04-19 18:27:17 -0500

    Moved `LocalFeedItem` to its own file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Pawel Klapuch on 4/19/23.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
