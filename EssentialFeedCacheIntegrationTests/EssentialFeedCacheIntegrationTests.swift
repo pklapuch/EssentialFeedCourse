46046c3a15823d8a01df2832d36e6d94c32abab6
Pawel Klapuch <pawelklapuch@gmail.com>
2023-04-28 15:50:53 -0500

    Fixed failing test

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -63,8 +63,12 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
     
     private func save(_ feed: [FeedImage], with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
         let saveExp = expectation(description: "Wait for save completion")
-        loader.save(feed) { saveError in
-            XCTAssertNil(saveError, "Expected to save feed successfully", file: file, line: line)
+        loader.save(feed) { result in
+            switch result {
+            case let .failure(error):
+                XCTFail("Expected to save feed successfullyL \(error)", file: file, line: line)
+            default: ()
+            }
             saveExp.fulfill()
         }
         wait(for: [saveExp], timeout: 1.0)
