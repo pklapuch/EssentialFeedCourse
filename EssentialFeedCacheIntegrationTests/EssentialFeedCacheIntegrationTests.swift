39b63ca6fce7df6c66de0fa25b76a5200235cb54
Pawel Klapuch <pawelklapuch@gmail.com>
2023-04-28 14:12:45 -0500

    Add `EssentialFeedCacheIntegrationTests` target to separate slow IO operation based tests from fast uni tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Pawel Klapuch on 4/28/23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+}
