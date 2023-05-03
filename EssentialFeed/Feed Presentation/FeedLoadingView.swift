4b0f7e69ce497ed4cbc15167e2a6008dc666f153
Pawel Klapuch <pawelklapuch@gmail.com>
2023-05-03 18:43:04 -0500

    Moved models and protocols into separate files

---
+++ EssentialFeed/Feed Presentation/FeedLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingView.swift
+//  EssentialFeed
+//
+//  Created by Pawel Klapuch on 5/3/23.
+//
+
+import Foundation
+
+public protocol FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel)
+}
---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Pawel Klapuch on 5/3/23.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
