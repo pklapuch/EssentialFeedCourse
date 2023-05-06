2eb7b796aae3344a89d16d459b650ab21537b977
Pawel Klapuch <pawelklapuch@gmail.com>
2023-05-06 10:16:10 -0500

    `RemoteFeedImageDataLoader.loadImageDataFromURL` delivers connectivity error on HTTP client error

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -15,14 +15,15 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     }
     
     public enum Error: Swift.Error {
+        case connectivity
         case invalidData
     }
     
     private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
         private var completion: ((FeedImageDataLoader.Result) -> Void)?
-
+        
         var wrapped: HTTPClientTask?
-
+        
         init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
             self.completion = completion
         }
@@ -46,15 +47,17 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         let task = HTTPClientTaskWrapper(completion)
         task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
-
+            
             switch result {
-            case let .success((data, response)):
+            case let .success(data, response):
                 if response.statusCode == 200, !data.isEmpty {
                     task.complete(with: .success(data))
                 } else {
                     task.complete(with: .failure(Error.invalidData))
                 }
-            case let .failure(error): task.complete(with: .failure(error))
+            
+            case .failure:
+                task.complete(with: .failure(Error.connectivity))
             }
         }
         return task
--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -28,11 +28,11 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
-    func test_loadImageDataFromURL_deliversErrorOnClientError() {
+    func test_loadImageDataFromURL_deliversConnectivityErrorOnClientError() {
         let (sut, client) = makeSUT()
         let clientError = NSError(domain: "a client error", code: 0)
 
-        expect(sut, toCompleteWith: .failure(clientError), when: {
+        expect(sut, toCompleteWith: failure(.connectivity), when: {
             client.complete(with: clientError)
         })
     }
@@ -113,7 +113,7 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         trackForMemoryLeaks(client, file: file, line: line)
         return (sut, client)
     }
-
+    
     private func failure(_ error: RemoteFeedImageDataLoader.Error) -> FeedImageDataLoader.Result {
         return .failure(error)
     }
@@ -144,4 +144,5 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+    
 }
