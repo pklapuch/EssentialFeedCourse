f5da0441e93cee24aab51e6087d889c822078da1
Pawel Klapuch <pawelklapuch@gmail.com>
2023-05-09 19:08:06 -0500

    Delivers fallback feed on primaryLoader failure

--- EssentialApp/EssentialAppTests/RemoteWithLocalFeedLoaderTests.swift
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFeedLoaderTests.swift
@@ -3,13 +3,23 @@ import EssentialFeed
 
 class FeedLoaderWithFallbackComposite: FeedLoader {
     private let primary: FeedLoader
+    private let fallback: FeedLoader
 
     init(primary: FeedLoader, fallback: FeedLoader) {
         self.primary = primary
+        self.fallback = fallback
     }
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load(completion: completion)
+        primary.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+                
+            case .failure:
+                self?.fallback.load(completion: completion)
+            }
+        }
     }
 }
 
@@ -33,6 +43,28 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+    
+    func test_load_deliversFallbackFeedOnPrimaryLoaderFailure() {
+        let fallbackFeed = uniqueFeed()
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { result in
+            switch result {
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed, fallbackFeed)
+
+            case .failure:
+                XCTFail("Expected successful load feed result, got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+
     
     // MARK: - Helpers
     
@@ -66,5 +98,9 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
     private func uniqueFeed() -> [FeedImage] {
         return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
 }
--- EssentialApp/EssentialAppTests/Tests.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-final class Tests: XCTestCase {
-    func test_zero() throws {
-        XCTFail("Tests not yet implemented in Tests")
-    }
-}
