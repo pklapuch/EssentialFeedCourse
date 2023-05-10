8c468531823f72e5ae4423120230d94bd5af042a
Pawel Klapuch <pawelklapuch@gmail.com>
2023-05-09 19:25:28 -0500

    Moved duplicated test method to shared helper

--- EssentialApp/EssentialAppTests/RemoteWithLocalFeedLoaderTests.swift
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFeedLoaderTests.swift
@@ -57,12 +57,6 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
                 
         wait(for: [exp], timeout: 1.0)
     }
-
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
-        }
-    }
     
     private class LoaderStub: FeedLoader {
         private let result: FeedLoader.Result
