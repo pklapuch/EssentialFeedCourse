c0f7c33bd8f93b17d6c8484e31f836da8537b268
Pawel Klapuch <pawelklapuch@gmail.com>
2023-05-12 14:11:30 -0500

    Extract `FeedLoaderStub` into shared scope to remove duplication

--- EssentialApp/EssentialAppTests/RemoteWithLocalFeedLoaderTests.swift
+++ EssentialApp/EssentialAppTests/RemoteWithLocalFeedLoaderTests.swift
@@ -28,8 +28,8 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
-        let primaryLoader = LoaderStub(result: primaryResult)
-        let fallbackLoader = LoaderStub(result: fallbackResult)
+        let primaryLoader = FeedLoaderStub(result: primaryResult)
+        let fallbackLoader = FeedLoaderStub(result: fallbackResult)
         let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
         trackForMemoryLeaks(primaryLoader, file: file, line: line)
         trackForMemoryLeaks(fallbackLoader, file: file, line: line)
@@ -56,18 +56,6 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         }
                 
         wait(for: [exp], timeout: 1.0)
-    }
-    
-    private class LoaderStub: FeedLoader {
-        private let result: FeedLoader.Result
-        
-        init(result: FeedLoader.Result) {
-            self.result = result
-        }
-
-        func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            completion(result)
-        }
     }
     
     private func uniqueFeed() -> [FeedImage] {
