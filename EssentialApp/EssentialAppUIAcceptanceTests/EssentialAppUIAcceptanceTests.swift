088e7a28148a30a73a6e4cd364a212f1f06e034c
Pawel Klapuch <pawelklapuch@gmail.com>
2023-05-29 09:32:55 -0500

    Added test target for Acceptance UI Tests: `EssentialAppUIAcceptanceTests`

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Pawel Klapuch on 5/29/23.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+    func test_zero() throws {
+    }
+}
