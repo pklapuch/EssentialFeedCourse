71517a623f32e1eab07a42cd5295f74d02b3d92b
Pawel Klapuch <pawelklapuch@gmail.com>
2023-06-02 15:27:32 -0500

    Moved duplicated test code to shared helper.

--- EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedItemsMapperTests.swift
@@ -80,9 +80,3 @@ class FeedItemsMapperTests: XCTestCase {
     }
 
 }
-
-private extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
-    }
-}
