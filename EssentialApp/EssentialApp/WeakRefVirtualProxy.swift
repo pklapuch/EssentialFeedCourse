97108409b75749f16012d2a5f885c9c94d611204
Pawel Klapuch <pawelklapuch@gmail.com>
2023-06-03 10:14:29 -0500

    Rename and move ResourceLoadingView and VIewModel to Shared module

--- EssentialApp/EssentialApp/WeakRefVirtualProxy.swift
+++ EssentialApp/EssentialApp/WeakRefVirtualProxy.swift
@@ -10,14 +10,20 @@ import EssentialFeed
 
 final class WeakRefVirtualProxy<T: AnyObject> {
     private weak var object: T?
-    
+
     init(_ object: T) {
         self.object = object
     }
 }
 
-extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
-    func display(_ viewModel: FeedLoadingViewModel) {
+extension WeakRefVirtualProxy: FeedErrorView where T: FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: ResourceLoadingView where T: ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel) {
         object?.display(viewModel)
     }
 }
@@ -25,11 +31,5 @@ extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
 extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
     func display(_ model: FeedImageViewModel<UIImage>) {
         object?.display(model)
-    }
-}
-
-extension WeakRefVirtualProxy: FeedErrorView where T: FeedErrorView {
-    func display(_ viewModel: FeedErrorViewModel) {
-        object?.display(viewModel)
     }
 }
