4ba36b6af1ac5126a8767f6219a5b70410ebe502
Pawel Klapuch <pawelklapuch@gmail.com>
2023-06-03 11:29:57 -0500

    Localize image comments title

---
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCommentsLocalizationTests.swift
+//  EssentialFeedTests
+//
+//  Created by Pawel Klapuch on 6/3/23.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsLocalizationTests: XCTestCase {
+
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+
+}
