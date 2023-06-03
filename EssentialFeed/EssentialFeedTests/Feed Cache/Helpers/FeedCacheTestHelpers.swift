feb0d66ebccb0eb7ff6594047ae8c4986862ce50
Pawel Klapuch <pawelklapuch@gmail.com>
2023-06-03 11:31:59 -0500

    Map ImageComments into ImageCommentsViewModels

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -15,26 +15,15 @@ func uniqueImage() -> FeedImage {
 func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
     let models = [uniqueImage(), uniqueImage()]
     let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-    
     return (models, local)
 }
 
 extension Date {
     func minusFeedCacheMaxAge() -> Date {
-        return self.adding(days: -feedCacheMaxAgeInDays)
+        return adding(days: -feedCacheMaxAgeInDays)
     }
-    
+
     private var feedCacheMaxAgeInDays: Int {
         return 7
-    }
-    
-    private func adding(days: Int) -> Date {
-        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-}
-
-extension Date {
-    func adding(seconds: TimeInterval) -> Date {
-        return self + seconds
     }
 }
