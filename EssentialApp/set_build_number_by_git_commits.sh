44dce09c0f3ce70ed8f88a46a4987b98f997e4c3
Pawel Klapuch <pawelklapuch@gmail.com>
2023-06-04 14:36:08 -0500

    Bump build version

--- EssentialApp/set_build_number_by_git_commits.sh
+++ EssentialApp/set_build_number_by_git_commits.sh
@@ -1,23 +1,3 @@
-#!/bin/bash
-
-git=$(sh /etc/profile; which git)
-number_of_commits=$("$git" rev-list HEAD --count)
-
-target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
-dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
-
-for plist in "$target_plist" "$dsym_plist"; do
-  if [ -f "$plist" ]; then
-    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${number_of_commits}" "$plist"
-  fi
-done
-
-settings_root_plist="$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Settings.bundle/Root.plist"
-
-if [ -f "$settings_root_plist" ]; then
-  settingsVersion="$APP_MARKETING_VERSION (${number_of_commits})"
-  /usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:1:DefaultValue $settingsVersion" "$settings_root_plist"
-else
-  echo "Could not find: $settings_root_plist"
-  exit 0
-fi
+buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+buildNumber=$(($buildNumber + 1))
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${PROJECT_DIR}/${INFOPLIST_FILE}"
